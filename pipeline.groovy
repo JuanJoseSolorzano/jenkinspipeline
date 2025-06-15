e303e42ca3081abe28fe6aef2098fe7442e85a94
juanjose <juanjose.solorzano.c@gmail.com>
2025-06-15 13:06:36 -0600

    Using HostMachine

--- pipeline.groovy
+++ pipeline.groovy
@@ -5,8 +5,8 @@
 // ==========================================================
 
 // The pipeline block defines the Jenkins Job configuration
-def REPOSITORY_URL = 'https://github.com/JuanJoseSolorzano/Bash_Scripting.git'
-def SEPARATOR = '$("="*75)'
+//def REPOSITORY_URL = 'https://github.com/JuanJoseSolorzano/Bash_Scripting.git'
+//def SEPARATOR = '$("="*75)'
 pipeline {
     agent none // No default agent to be able to use matrix agents and execute the same job on multiple agents
     stages {
@@ -26,9 +26,7 @@ pipeline {
                     }
                     stage("Cloning a repository"){
                         powershell """
-                            Write-Output 'Cloning the repository ${REPOSITORY_URL} using the agent ${AGENTS_LABEL}' >> log.txt
-                            Write-Output ${SEPARATOR} >> log.txt
-                            git clone ${REPOSITORY_URL} >> log.txt
+                            git clone https://github.com/JuanJoseSolorzano/Bash_Scripting.git >> log.txt
                         """
                     }
                 }

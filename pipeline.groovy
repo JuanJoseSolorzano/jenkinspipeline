300c0190ed01a9056032ea7dc819301a05dba1e8
juanjose <juanjose.solorzano.c@gmail.com>
2025-06-15 12:34:40 -0600

    Using HostMachine

--- pipeline.groovy
+++ pipeline.groovy
@@ -7,13 +7,16 @@
 // The pipeline block defines the Jenkins Job configuration
 
 pipeline {
-    agent {
-        label 'WindowsHostMachine'
-    }
+    agent none //
     stages {
-        stage("Testing"){
-            steps {
-                powershell 'Write-Host "This is a test stage running on a Windows host machine."'
+        stage("Configure Agents"){
+            matrix {
+                axes {
+                    axis {
+                        name 'Agents_Labels'
+                        valus 'WindowsHostMachine','WindowsHostMachine1','WindowsHostMachine2'
+                    }
+                }
             }
         }
     }
